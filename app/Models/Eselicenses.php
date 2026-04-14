8049d977ecbf5866d1d3b7cff26fdc911eac89d1
Vivek Raj Gupta <nehavivekshah@gmail.com>
2026-04-14 17:26:35 +0530

    .

--- app/Models/Eselicenses.php
+++ app/Models/Eselicenses.php
@@ -10,5 +10,22 @@ use App\Traits\BelongsToCompany;
 class Eselicenses extends Model
 {
     use HasFactory, BelongsToCompany;
-    
+
+    protected $fillable = [
+        'cid',
+        'project_id',
+        'client_name',
+        'company',
+        'mob',
+        'email',
+        'project_name',
+        'type',
+        'amount',
+        'deployment_url',
+        'technology_stack',
+        'note',
+        'eselicense_key',
+        'expiry_date',
+        'status',
+    ];
 }
